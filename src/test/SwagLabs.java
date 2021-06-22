df3e177bfe0d789dfdb5dee14eab004b3c0b2a93
Mustafa19061 <86225840+Mustafa19061@users.noreply.github.com>
2021-06-23 00:53:50 +0530

    Update SwagLabs.java

--- src/test/SwagLabs.java
+++ src/test/SwagLabs.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class SauceLabs extends  BaseClass  {
+public class SwagLabs extends  BaseClass  {
 
 	 @Test
     public void testOne() throws InterruptedException {
