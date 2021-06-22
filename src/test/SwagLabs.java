4c2e21cf071349ec443146e1c677e37859542133
Mustafa19061 <86225840+Mustafa19061@users.noreply.github.com>
2021-06-23 00:52:10 +0530

    Update and rename SauceLabs.java to SwagLabs.java

--- src/test/SwagLabs.java
+++ src/test/SwagLabs.java
@@ -11,28 +11,11 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 
-public class SauceLabs {
+public class SauceLabs extends  BaseClass  {
 
-	  public static final String URL = "http://127.0.0.1:4723/wd/hub";
-	 
-	  public static void main(String[] args) throws Exception {
-	 
- 
-		DesiredCapabilities caps = new DesiredCapabilities();
-		caps.setCapability("platform", "Android");
-		caps.setCapability("platform", "Android");
-		caps.setCapability("platform", "Android");
-		caps.setCapability("version", "9.1");
-		caps.setCapability("deviceName", "Emulator");
-		caps.setCapability("appPackage", "com.Android.SauceLabs.Mobile.Sample.app");
-		caps.setCapability(MobileCapabilityType.APP, "D:\\Work\\Selenium\\Appium\\Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
-		caps.setCapability("appActivity", ".MainActivity");
-		WebDriver driver = new RemoteWebDriver(new java.net.URL(URL), caps);
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-	
-		//invoke .exe file
-		System.setProperty("webdriverchrome.driver", "Drivers\\Chromedriver.exe");
-	 
+	 @Test
+    public void testOne() throws InterruptedException {
+ 	 
 		//Go to the login screen
 		driver.findElement(By.id("Login Screen")).click();
 		driver.findElement(By.name("Enter your name")).sendKeys("Mustafa"); 
@@ -40,18 +23,39 @@ public class SauceLabs {
 		driver.findElement(By.className("android.saucelabapp.EditText")).sendKeys("@chjshyy"); 
 		
 		driver.findElement(By.xpath("//android.saucelab.Button[@index='1']")).click();
+	     MobileElement productPage = driver.findElement(By.id("prd_page"));
+
+        List<MobileElement> elements = driver.findElements(By.className("android.widget.PageView"));
+        List<MobileElement> srpElements = driver.findElements(By.className("android.widget.RelativeLayout"));
+
+        for (int i = 0; i < elements.size(); i++) {
+            String str1 = elements.get(i).getText().toString();
+            System.out.println(str1.toLowerCase());
+            Assert.assertTrue(str1.toLowerCase().contains(patternSearchTxt1));
+        }
+
+        srpElements.get(0).click();
 		 
 		//Get the title and print it
-		System.out.println(driver.getTitle());
+		
+	      String actualTitle = driver.getTitle();
+              String expectedTitle = "Swag Labs";
+              Assert.assertEquals(expectedTitle,actualTitle);
+
 		
 		//Print page source
 		System.out.println(driver.getPageSource());
-			
+	    
+    }
+	
+	 @Test
+    public void testTwo() throws InterruptedException {
+	
 		 // Selecting first product
 	    driver.findElement(By
 	        .xpath("//android.sauceLabs.Products[@text-'SauceLabs']"));
 	    String name = driver.findElement(By.className("title")).getText();
-	    
+	    MobileElement logoview;
 
 	    //Adding selected product to cart
 	    driver.findElement(By.xpath("//android.sauceLabs.Cart[@id='Add to Cart']")).click();
@@ -60,10 +64,9 @@ public class SauceLabs {
 	    //Checking product is available in cart or not
 	    driver.findElement(By.xpath("//android.sauceLabs.Cart[@class='cart-label']")).click();
 	  
-			
-		//Close all the browsers
-		driver.quit();
-
+	 Assert.assertTrue(logoview.isEnabled());
+       
+		
 	}
 
 }
